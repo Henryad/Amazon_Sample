e60490cead092acde54e932e1b7e65dd472721e5
Radamanthys <72035719+Henryad@users.noreply.github.com>
2022-10-26 16:28:02 -0300

    First Commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'sa-east-1_GbqOv7lU7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '17seio5gonra366eidfe6jhl55', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'sa-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://1ilxlgzweh.execute-api.sa-east-1.amazonaws.com/prod' //
+    }
+};
